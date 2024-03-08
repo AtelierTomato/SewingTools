74be5a053dc8d56cadff292569253e6b7700d1e4
suflors <iammagical.space@gmail.com>
2024-03-07 17:16:52 -0800

    Added database models for Fabric, FabricInImage, FabricSquare, FabricTriangle, and Image

---
+++ SewingTools/SewingTools/Database/Model/FabricTriangle.cs
@@ -0,0 +1,10 @@
+﻿namespace SewingTools.Database.Model
+{
+	public record FabricTriangle(
+		ulong FabricID,
+		int ShortestLength,
+		int MiddleLength,
+		int LongestLength,
+		int Amount = 1
+	);
+}
---
+++ SewingTools/SewingTools/Database/Model/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace SewingTools.Database.Model
+{
+	public record Image(
+		ulong ID,
+		string FileExtension,
+		string Title,
+		string Description
+	);
+}
