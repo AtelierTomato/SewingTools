ebe9f1caa74f07d4edf71a62b27337467d5ff9f5
suflors <iammagical.space@gmail.com>
2024-03-07 18:50:53 -0800

    Implemented SqliteFabricAccess

--- AtelierTomato.Sewing/AtelierTomato.SewingTools/Database/IFabricAccess.cs
+++ AtelierTomato.Sewing/AtelierTomato.SewingTools/Database/IFabricAccess.cs
@@ -5,7 +5,7 @@ namespace AtelierTomato.SewingTools.Database
 	public interface IFabricAccess
 	{
 		Task<Fabric> ReadFabric(ulong ID);
-		Task<IEnumerable<Fabric>> ReadFabricRange(IEnumerable<ulong> ID);
+		Task<IEnumerable<Fabric>> ReadFabricRange(IEnumerable<ulong> IDs);
 		Task<IEnumerable<Fabric>> ReadAllFabrics();
 		Task WriteFabric(Fabric fabric);
 		Task WriteFabricRange(IEnumerable<Fabric> fabrics);
---
+++ AtelierTomato.Sewing/AtelierTomato.SewingTools/Database/Sqlite/SqliteFabricAccess.cs
@@ -0,0 +1,78 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using SewingTools.Database.Model;
+
+namespace AtelierTomato.SewingTools.Database.Sqlite
+{
+	public class SqliteFabricAccess : IFabricAccess
+	{
+		private string connectionString = "Data Source=K:\\Projects\\Programming\\,Databases";
+
+		public async Task<IEnumerable<Fabric>> ReadAllFabrics()
+		{
+			await using var connection = new SqliteConnection(connectionString);
+			connection.Open();
+
+			var result = await connection.QueryAsync<Fabric>($@"select * from {nameof(Fabric)}");
+
+			connection.Close();
+
+			return result;
+		}
+
+		public async Task<Fabric> ReadFabric(ulong ID)
+		{
+			await using var connection = new SqliteConnection(connectionString);
+			connection.Open();
+
+			var result = await connection.QuerySingleAsync<Fabric>($@"select * from {nameof(Fabric)} where {nameof(Fabric.ID)} = @ID", new { ID });
+
+			connection.Close();
+
+			return result;
+		}
+
+		public async Task<IEnumerable<Fabric>> ReadFabricRange(IEnumerable<ulong> IDs)
+		{
+			await using var connection = new SqliteConnection(connectionString);
+			connection.Open();
+
+			var result = await connection.QueryAsync<Fabric>($@"select * from {nameof(Fabric)} where {nameof(Fabric.ID)} IN @IDs", new { IDs });
+
+			connection.Close();
+
+			return result;
+		}
+
+		public async Task WriteFabric(Fabric fabric) => await WriteFabricRange([fabric]);
+
+		public async Task WriteFabricRange(IEnumerable<Fabric> fabrics)
+		{
+			await using var connection = new SqliteConnection(connectionString);
+			connection.Open();
+			foreach (Fabric fabric in fabrics)
+			{
+				await WriteFabricCore(fabric, connection);
+			}
+
+			connection.Close();
+		}
+
+		private static async Task WriteFabricCore(Fabric fabric, SqliteConnection connection)
+		{
+			await connection.ExecuteAsync($@"
+insert into {nameof(Fabric)} ( {nameof(Fabric.ID)}, {nameof(Fabric.Name)}, {nameof(Fabric.Material)})
+Values ( @ID, @name, @material )
+on conflict ({nameof(Fabric.ID)}) do update set
+{nameof(Fabric.Name)} = excluded.{nameof(Fabric.Name)},
+{nameof(Fabric.Material)} = excluded.{nameof(Fabric.Material)}
+",
+			new
+			{
+				ID = fabric.ID,
+				name = fabric.Name,
+				material = fabric.Material
+			});
+		}
+	}
+}
