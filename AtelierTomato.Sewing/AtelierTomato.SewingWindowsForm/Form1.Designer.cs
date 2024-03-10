127c4806f8814a252a1024b0ed9b9936c546d3b5
suflors <iammagical.space@gmail.com>
2024-03-09 17:59:44 -0800

    added placeholder buttons to fabric page

--- AtelierTomato.Sewing/AtelierTomato.SewingWindowsForm/Form1.Designer.cs
+++ AtelierTomato.Sewing/AtelierTomato.SewingWindowsForm/Form1.Designer.cs
@@ -32,6 +32,12 @@
 			buttonFabric = new Button();
 			buttonImage = new Button();
 			buttonReturn = new Button();
+			addFabricButton = new Button();
+			editFabricButton = new Button();
+			addSquareButton = new Button();
+			removeSquareButton = new Button();
+			addTriangleButton = new Button();
+			removeTriangleButton = new Button();
 			SuspendLayout();
 			// 
 			// title
@@ -66,10 +72,10 @@
 			buttonImage.UseVisualStyleBackColor = true;
 			buttonImage.Click += imageButtonClick;
 			// 
-			// returnButton
+			// buttonReturn
 			// 
 			buttonReturn.Location = new Point(350, 413);
-			buttonReturn.Name = "returnButton";
+			buttonReturn.Name = "buttonReturn";
 			buttonReturn.Size = new Size(100, 25);
 			buttonReturn.TabIndex = 3;
 			buttonReturn.Text = "return";
@@ -78,11 +84,77 @@
 			buttonReturn.Visible = false;
 			buttonReturn.Click += returnButtonClick;
 			// 
+			// addFabricButton
+			// 
+			addFabricButton.Location = new Point(197, 144);
+			addFabricButton.Name = "addFabricButton";
+			addFabricButton.Size = new Size(200, 50);
+			addFabricButton.TabIndex = 4;
+			addFabricButton.Text = "add fabric";
+			addFabricButton.UseVisualStyleBackColor = true;
+			addFabricButton.Visible = false;
+			// 
+			// editFabricButton
+			// 
+			editFabricButton.Location = new Point(403, 144);
+			editFabricButton.Name = "editFabricButton";
+			editFabricButton.Size = new Size(200, 50);
+			editFabricButton.TabIndex = 5;
+			editFabricButton.Text = "edit fabric";
+			editFabricButton.UseVisualStyleBackColor = true;
+			editFabricButton.Visible = false;
+			// 
+			// addSquareButton
+			// 
+			addSquareButton.Location = new Point(197, 200);
+			addSquareButton.Name = "addSquareButton";
+			addSquareButton.Size = new Size(200, 50);
+			addSquareButton.TabIndex = 6;
+			addSquareButton.Text = "add fabric square";
+			addSquareButton.UseVisualStyleBackColor = true;
+			addSquareButton.Visible = false;
+			// 
+			// removeSquareButton
+			// 
+			removeSquareButton.Location = new Point(403, 200);
+			removeSquareButton.Name = "removeSquareButton";
+			removeSquareButton.Size = new Size(200, 50);
+			removeSquareButton.TabIndex = 7;
+			removeSquareButton.Text = "remove fabric square";
+			removeSquareButton.UseVisualStyleBackColor = true;
+			removeSquareButton.Visible = false;
+			// 
+			// addTriangleButton
+			// 
+			addTriangleButton.Location = new Point(197, 256);
+			addTriangleButton.Name = "addTriangleButton";
+			addTriangleButton.Size = new Size(200, 50);
+			addTriangleButton.TabIndex = 8;
+			addTriangleButton.Text = "add fabric triangle";
+			addTriangleButton.UseVisualStyleBackColor = true;
+			addTriangleButton.Visible = false;
+			// 
+			// removeTriangleButton
+			// 
+			removeTriangleButton.Location = new Point(403, 256);
+			removeTriangleButton.Name = "removeTriangleButton";
+			removeTriangleButton.Size = new Size(200, 50);
+			removeTriangleButton.TabIndex = 9;
+			removeTriangleButton.Text = "remove fabric triangle";
+			removeTriangleButton.UseVisualStyleBackColor = true;
+			removeTriangleButton.Visible = false;
+			// 
 			// Form1
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(800, 450);
+			Controls.Add(removeTriangleButton);
+			Controls.Add(addTriangleButton);
+			Controls.Add(removeSquareButton);
+			Controls.Add(addSquareButton);
+			Controls.Add(editFabricButton);
+			Controls.Add(addFabricButton);
 			Controls.Add(buttonReturn);
 			Controls.Add(buttonImage);
 			Controls.Add(buttonFabric);
@@ -99,5 +171,11 @@
 		private Button buttonFabric;
 		private Button buttonImage;
 		private Button buttonReturn;
+		private Button addFabricButton;
+		private Button editFabricButton;
+		private Button addSquareButton;
+		private Button removeSquareButton;
+		private Button addTriangleButton;
+		private Button removeTriangleButton;
 	}
 }
--- AtelierTomato.Sewing/AtelierTomato.SewingWindowsForm/Form1.cs
+++ AtelierTomato.Sewing/AtelierTomato.SewingWindowsForm/Form1.cs
@@ -13,6 +13,12 @@ namespace AtelierTomato.SewingWindowsForm
 			title.Left = (ClientSize.Width / 2) - (title.Width / 2);
 			buttonFabric.Visible = false;
 			buttonImage.Visible = false;
+			addFabricButton.Visible = true;
+			editFabricButton.Visible = true;
+			addSquareButton.Visible = true;
+			removeSquareButton.Visible = true;
+			addTriangleButton.Visible = true;
+			removeTriangleButton.Visible = true;
 			buttonReturn.Visible = true;
 		}
 
@@ -31,6 +37,12 @@ namespace AtelierTomato.SewingWindowsForm
 			title.Left = (ClientSize.Width / 2) - (title.Width / 2);
 			buttonFabric.Visible = true;
 			buttonImage.Visible = true;
+			addFabricButton.Visible = false;
+			editFabricButton.Visible = false;
+			addSquareButton.Visible = false;
+			removeSquareButton.Visible = false;
+			addTriangleButton.Visible = false;
+			removeTriangleButton.Visible = false;
 			buttonReturn.Visible = false;
 		}
 	}
